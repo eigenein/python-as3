c270938853e4d43dc11050dbaa9b58032d982c3a
Pavel Perestoronin <eigenein@gmail.com>
2018-08-28 22:10:55 +0200

    🔧 Shorter type annotations

--- as3/ast_.py
+++ as3/ast_.py
@@ -2,6 +2,7 @@
 AST helpers.
 """
 import ast
+from ast import AST
 from typing import Callable, List, Optional, Tuple
 
 import as3.parser
@@ -9,11 +10,11 @@ from as3.constants import init_name
 from as3.scanner import Token, TokenType
 
 
-def make_ast(token: Token, init: Callable[..., ast.AST], **kwargs) -> ast.AST:
+def make_ast(token: Token, init: Callable[..., AST], **kwargs) -> AST:
     return init(**kwargs, lineno=token.line_number, col_offset=token.position)
 
 
-def make_ast_from_source(token: Token, source: str) -> ast.AST:
+def make_ast_from_source(token: Token, source: str) -> AST:
     # FIXME: I don't really like to use this function so replace with something better.
     node, = ast.parse(source).body
     node.lineno = token.line_number
@@ -21,23 +22,23 @@ def make_ast_from_source(token: Token, source: str) -> ast.AST:
     return ast.fix_missing_locations(node)
 
 
-def make_name(name_token: Token, custom_name: Optional[str] = None, ctx=ast.Load()) -> ast.AST:
+def make_name(name_token: Token, custom_name: Optional[str] = None, ctx=ast.Load()) -> AST:
     assert custom_name or name_token.type_ == TokenType.IDENTIFIER
     return make_ast(name_token, ast.Name, id=(custom_name or name_token.value), ctx=ctx)
 
 
-def make_call(call_token: Token, func: ast.AST, args: List[ast.AST] = None) -> ast.AST:
+def make_call(call_token: Token, func: AST, args: List[AST] = None) -> AST:
     return make_ast(call_token, ast.Call, func=func, args=(args or []), keywords=[])
 
 
 def make_function(
     function_token: Token,
     name: str,
-    body: List[ast.AST],
-    args: List[ast.AST] = None,
+    body: List[AST],
+    args: List[AST] = None,
     returns=None,
-    decorator_list: List[ast.AST] = None,
-) -> ast.AST:
+    decorator_list: List[AST] = None,
+) -> AST:
     return make_ast(
         function_token,
         ast.FunctionDef,
@@ -49,7 +50,7 @@ def make_function(
     )
 
 
-def make_initializer(class_token: Token, field_values: List[Tuple[Token, ast.AST]]) -> ast.AST:
+def make_initializer(class_token: Token, field_values: List[Tuple[Token, AST]]) -> AST:
     """
     Make `__init__` function for a class.
     """
@@ -74,7 +75,7 @@ def make_initializer(class_token: Token, field_values: List[Tuple[Token, ast.AST
     )
 
 
-def set_store_context(node: ast.AST, assignment_token: Token) -> ast.AST:
+def set_store_context(node: AST, assignment_token: Token) -> AST:
     if not hasattr(node, 'ctx'):
         as3.parser.raise_syntax_error(f"{ast.dump(node)} can't be assigned to", assignment_token)
     node.ctx = ast.Store()
