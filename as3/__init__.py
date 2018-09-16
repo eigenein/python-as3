7d676910648cf82b031ece2f587158bec8aed878
Pavel Perestoronin <eigenein@gmail.com>
2018-09-16 17:48:42 +0200

    🎉 `evaluate_expression`

--- as3/__init__.py
+++ as3/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from ast import AST
+from ast import AST, Expression
 from types import CodeType
 from typing import Any, Dict
 
@@ -9,16 +9,32 @@ from as3.runtime import default_globals
 from as3.scanner import scan
 
 
+def parse(source: str) -> Parser:
+    return Parser(scan(source))
+
+
 def parse_script(source: str) -> AST:
-    return Parser(scan(source)).parse_script()
+    return parse(source).parse_script()
 
 
-def compile_script(source: str, filename: str) -> CodeType:
+def compile_script(source: str, filename: str = '<ast>') -> CodeType:
     return compile(parse_script(source), filename, 'exec')
 
 
-def execute_script(source: str, filename: str, **override_globals: Any) -> Dict[str, Any]:
-    code = compile_script(source, filename)
+def execute_script(source: str, filename: str = '<ast>', **override_globals: Any) -> Dict[str, Any]:
     globals_ = {**default_globals, **override_globals}
-    exec(code, globals_)
+    exec(compile_script(source, filename), globals_)
     return globals_
+
+
+def parse_expression(source: str) -> AST:
+    return parse(source).parse_expression()
+
+
+def compile_expression(source: str, filename: str = '<ast>') -> CodeType:
+    return compile(Expression(parse_expression(source)), filename, 'eval')
+
+
+def evaluate_expression(source: str, filename: str = '<ast>', **override_globals: Any) -> Dict[str, Any]:
+    globals_ = {**default_globals, **override_globals}
+    return eval(compile_expression(source, filename), globals_)
--- tests/tests.py
+++ tests/tests.py
@@ -1,26 +1,23 @@
 from __future__ import annotations
 
-from ast import Expression
 from collections import namedtuple
 from typing import Any, Dict
 
 from pytest import mark, param, raises
 
-from as3 import default_globals, execute_script
+from as3 import evaluate_expression, execute_script, parse_expression
 from as3.examples import bad_scripts, expressions, scripts
 from as3.exceptions import ASSyntaxError
-from as3.parser import Parser
-from as3.scanner import scan
 
 
 @mark.parametrize('expression, expected', expressions)
 def test_evaluate_expression(expression: str, expected: Any):
-    actual = eval(compile(Expression(Parser(scan(expression)).parse_expression()), '<ast>', 'eval'), {
-        **default_globals,
-        'foo': namedtuple('Foo', 'bar baz')(bar=42, baz=2),
-        'bar': lambda a, b: a + b,
-        'baz': lambda: 42,
-    })
+    actual = evaluate_expression(
+        expression,
+        foo=namedtuple('Foo', 'bar baz')(bar=42, baz=2),
+        bar=lambda a, b: a + b,
+        baz=lambda: 42,
+    )
     assert type(actual) == type(expected), f'actual: {actual!r}'
     assert actual == expected, f'actual: {actual!r}'
 
@@ -33,12 +30,12 @@ def test_evaluate_expression(expression: str, expected: Any):
 ])
 def test_parse_expression_syntax_error(expression: str):
     with raises(ASSyntaxError):
-        Parser(scan(expression)).parse_expression()
+        parse_expression(expression)
 
 
 @mark.parametrize('script, expected', scripts + bad_scripts)
 def test_execute_script(script: str, expected: Dict[str, Any]):
-    globals_ = execute_script(script, '<ast>', foo=lambda x: x, dict_={})
+    globals_ = execute_script(script, foo=lambda x: x, dict_={})
     for key, value in expected.items():
         assert globals_[key] == value, f'actual: {globals_[key]}'
 
@@ -50,4 +47,4 @@ def test_execute_script(script: str, expected: Dict[str, Any]):
 ])
 def test_execute_script_name_error(script: str):
     with raises(NameError):
-        execute_script(script, '<ast>')
+        execute_script(script)
